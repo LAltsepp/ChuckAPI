c556de606004e84055b0ade7b91660b0db672a42
Opilane TTHK <opilane@tthk.ee>
2022-01-19 10:52:59 +0200

    first commit

---
+++ ChuckNorris.API/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ChuckNorris.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            GetJokes();
+        }
+
+        public static void GetJokes()
+        {
+            string url = "https://api.icndb.com/jokes/random";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using(var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+                Console.WriteLine(joke.value);
+            }
+        }
+    }
+}
